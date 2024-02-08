35a25dd9304e642a9a67c532ba118aff3f2ecbfe
vinod sjml <vinsjml@gmail.com>
2024-02-08 11:13:11 +0530

    push

--- sample-vm/vm.tf
+++ sample-vm/vm.tf
@@ -1,10 +1,35 @@
 resource "aws_instance" "web"{
-  ami           = "data"
-  instance_type = "t2.micro"
+  ami               = "data"
+  instance_type     = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
     tags = {
       Name = "tf-test"
     }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    description = "ssh from internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
 
 
